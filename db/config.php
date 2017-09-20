6411bf9f2c8389ac8a329777c5048d696d8cdb08
Peter Horrack <phorrack@casaria.net>
2017-09-20 13:38:25 -0700

    updaTE DB

--- db/config.php
+++ db/config.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    "db" => "mysql:host=127.0.0.1;dbname=jsgridsample",
-    "username" => "root",
-    "password" => "mysql"
+    "db" => "mysql:host=127.0.0.1;dbname=casaria_hdesk1",
+    "username" => "casaria_hdesk1",
+    "password" => "5XwoR]B"
 );
 
 ?>
